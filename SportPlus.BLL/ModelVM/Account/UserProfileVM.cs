d095b1c091ae2c70f72934f0d111c4755980f10c
Mohamed <hamom5075@gmail.com>
2024-11-27 00:49:35 +0200

    Adding ModelVM and Update on AccountVM and UserVM

---
+++ SportPlus.BLL/ModelVM/Account/UserProfileVM.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+
+
+namespace SportPlus.BLL.ModelVM.Account
+{
+    public class UserProfileVM
+    {
+
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime DOB { get; set; }
+        public string Gender { get; set; }
+        public string NationalId { get; set; }
+        public string ProfileImage { get; set; } // Existing profile image
+        public IFormFile NewProfileImage { get; set; } // For uploading a new image
+    }
+}
