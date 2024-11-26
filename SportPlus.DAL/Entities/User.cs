e9a8338a72dd0844c1c7c822978190c0e65d1f00
Mohamed <hamom5075@gmail.com>
2024-11-26 16:25:59 +0200

    the last update

---
+++ SportPlus.DAL/Entities/User.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    [Table("User")]
+    public class User : IdentityUser
+    {
+        [Required]
+        [MaxLength(30, ErrorMessage = "Name must be less than 30 letter")]
+        [MinLength(3, ErrorMessage = "NAme must be greater than 3 letter")]
+        public string FullName { get; set; } = string.Empty;
+        [Required]
+        [RegularExpression(@"(Male|Famale)", ErrorMessage = "Gender Must be Male or Fmale")]
+        public string Gender { get; set; } = string.Empty;
+
+        public string? Address { get; set; }
+        public string? City { get; set; }
+        public string? Image { get; set; }
+        public DateTime DOB { get; set; } = DateTime.Now;
+        public string FavoriteTeam { get; set; }
+    }
+}
+}
