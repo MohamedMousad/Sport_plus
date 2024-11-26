3a30ff125cadd1fadde49d9d8e1980f22c029f4c
Mohamed <hamom5075@gmail.com>
2024-11-26 16:25:59 +0200

    the last update

---
+++ SportPlus.DAL/DB/ApplicationDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+
+namespace SportPlus.DAL.DB
+{
+        public class ApplicationDBContext : IdentityDbContext<User>
+        {
+            public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+            {
+            }
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                base.OnModelCreating(modelBuilder);
+                modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            }
+        }
+    }
+}
