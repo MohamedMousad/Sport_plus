25c15f428844921d89f5cf116d90a5dded2ab769
Mohamed <hamom5075@gmail.com>
2024-11-27 00:49:35 +0200

    Adding ModelVM and Update on AccountVM and UserVM

--- SportPlus.BLL/Service/Abstraction/IAccountService.cs
+++ SportPlus.BLL/Service/Abstraction/IAccountService.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using SportPlus.BLL.ModelVM.Account;
+using SportPlus.BLL.ModelVM.User;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,23 +8,25 @@ using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
 namespace SportPlus.BLL.Service.Abstraction
 {
     public interface IAccountService
     {
-        Task<UserProfileVM> GetProfile(ClaimsPrincipal user);
+        Task<ModelVM.Account.UserProfileVM> GetProfile(ClaimsPrincipal user);
         Task<EditeUserVM> GetUserForEdit(ClaimsPrincipal user);
         Task<UserVM> GetUserForMoreInfo(ClaimsPrincipal user);
         Task<IdentityResult> UpdateUser(ClaimsPrincipal user, EditeUserVM model);
-        Task<User> GetCurrentUser(ClaimsPrincipal user);
+        Task<Entities.User> GetCurrentUser(ClaimsPrincipal user);
         Task<IdentityResult> DeleteUserAccount(ClaimsPrincipal user);
-        Task<IdentityResult> ChangePassword(ChangePasswordVM model);
+        Task<IdentityResult> ChangePassword(ChangePasswordAccountVM model);
         // Task<User> GetCurrentUser();
-        Task<IdentityResult> UpdateUser(User User);
+        Task<IdentityResult> UpdateUser(Entities.User User);
         Task<LoginVM> GetLoginViewModelAsync();
         Task<SignInResult> Login(LoginVM loginVM);
         Task<IdentityResult> RegisterUserAsync(RegistrationVM registerVM);
         Task Logout();
-        Task<bool> IsLockedOut(User User);
+        Task<bool> IsLockedOut(Entities.User User);
     }
 }
