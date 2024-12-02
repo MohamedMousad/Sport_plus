26494dc7d4d517a1831e56ed8f175f64cd18a27e
Mohamed <hamom5075@gmail.com>
2024-12-02 16:43:40 +0200

    the last version of project

--- SportPlus.DAL/DB/ApplicationDBContext.cs
+++ SportPlus.DAL/DB/ApplicationDBContext.cs
@@ -2,20 +2,23 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System.Reflection;
+using System.Data.Entity;
 
 
 namespace SportPlus.DAL.DB
 {
-        public class ApplicationDbContext : IdentityDbContext<User>
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-            public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-            {
-            }
-            protected override void OnModelCreating(ModelBuilder modelBuilder)
-            {
-                base.OnModelCreating(modelBuilder);
-                modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
-            }
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+    public System.Data.Entity.DbSet<User> Users { get; set; }
+    } 
 }
 
